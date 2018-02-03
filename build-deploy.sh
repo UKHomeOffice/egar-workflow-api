7385a9cb2a5e1fbe2c292b789d69c04c52e2bf91
PenfoldG <gareth.penfold@ipl.com>
2018-02-03 09:14:36 +0000

    Updating the build scripts

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 # Build and deploy the Maven code to S3
-export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$ACCESS_KEY
  
 ./scripts/bash/install-egar-parent.sh 8064aaac1efeaefc9cac19fb88adfb5baade5d20
 ./scripts/bash/install-egar-common.sh e035a55a26a0e9d4b078571b0b75ae2a70338cae
  
 mvn -B compile
 mvn -B test
-mvn -B install -DskipTests
+mvn -B package -DskipTests
